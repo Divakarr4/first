18d10ba73cdef553a6f2e07de197db33deb26381
Divakarr4 <divakarsetty5@gmail.com>
2023-11-11 09:52:24 +0530

    dom practice

---
+++ dom.js
@@ -0,0 +1,25 @@
+//EXAMINE THE DOCUMENT OBJECT
+
+//console.dir(document)
+
+// console.log(document.url);
+// console.log(document.title);
+
+document.title="Cart";
+// document.all[10].textContent='hello';
+// console.log(document.forms);
+
+var headerTitle = document.getElementById('header-title');
+
+var header = document.getElementById('main-header');
+
+var additems = document.getElementById('additems');
+console.log(headerTitle);
+
+headerTitle.textContent='Hello please add items';
+headerTitle.style.color= 'blue';
+
+header.style.borderBottom = 'solid 6px black'
+
+additems.style.color='green';
+additems.style.fontWeight='bold';
