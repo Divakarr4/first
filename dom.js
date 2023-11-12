4e28b2c9b6a6829b2f187342888a9f4c83bf65f3
Divakarr4 <divakarsetty5@gmail.com>
2023-11-12 11:36:33 +0530

    style changes added

--- dom.js
+++ dom.js
@@ -1,25 +1,36 @@
-//EXAMINE THE DOCUMENT OBJECT
+// //EXAMINE THE DOCUMENT OBJECT
 
-//console.dir(document)
+// //console.dir(document)
 
-// console.log(document.url);
-// console.log(document.title);
+// // console.log(document.url);
+// // console.log(document.title);
 
-document.title="Cart";
-// document.all[10].textContent='hello';
-// console.log(document.forms);
+// document.title="Cart";
+// // document.all[10].textContent='hello';
+// // console.log(document.forms);
 
-var headerTitle = document.getElementById('header-title');
+// var headerTitle = document.getElementById('header-title');
 
-var header = document.getElementById('main-header');
+// var header = document.getElementById('main-header');
 
-var additems = document.getElementById('additems');
-console.log(headerTitle);
+// var additems = document.getElementById('additems');
+// console.log(headerTitle);
 
-headerTitle.textContent='Hello please add items';
-headerTitle.style.color= 'blue';
+// headerTitle.textContent='Hello please add items';
+// headerTitle.style.color= 'blue';
 
-header.style.borderBottom = 'solid 6px black'
+// header.style.borderBottom = 'solid 6px black'
 
-additems.style.color='green';
-additems.style.fontWeight='bold';
+// additems.style.color='green';
+// additems.style.fontWeight='bold';
+
+let items = document.getElementsByClassName('list-group-item');
+
+items[2].style.backgroundColor='green';
+
+for(var i=0; i < items.length;i++){
+     items[i].style.fontWeight='bold';
+ }
+
+
+
